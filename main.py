d9f3db3ab2bce7edc2b0e4b47540e39636de19df
Shanu Mathew <shanutmathew1703@gmail.com>
2023-10-07 00:16:34 +0530

    Completed Project

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import cv2
+import cvzone
 from cvzone.HandTrackingModule import HandDetector
+from time import sleep
+import numpy as np
+
 
 cap=cv2.VideoCapture(0)
 if not cap.isOpened():
@@ -12,24 +16,34 @@ detector=HandDetector(detectionCon=0.8)
 keys= [["Q","W","E","R","T","Y","U","I","O","P"],
        ["A","S","D","F","G","H","J","K","L",";"],
        ["Z","X","C","V","B","N","M",",",".","/"]]
+finalText=""
+
 
 
 def drawALL(img,buttonList):
-    
+    imgNew=np.zeros_like(img,np.uint8)
     for button in buttonList:
         x,y=button.pos
+        cvzone.cornerRect(imgNew,(button.pos[0],button.pos[1],button.size[0],button.size[1]),
+                          50,rt=0,colorC=(10,10,10))
         w,h=button.size
-        cv2.rectangle(img,button.pos,(x+w,y+h),(255,0,255),cv2.FILLED)
-        cv2.putText(img,button.text,(x+8,y+40),
+        cv2.rectangle(imgNew,button.pos,(x+w,y+h),(50,50,50),cv2.FILLED)
+        cv2.putText(imgNew,button.text,(x+8,y+40),
                 cv2.FONT_HERSHEY_PLAIN,
-                3,(0,0,0),3)
-    return img
+                3,(255,255,255),3)
+    out=img.copy()
+    alpha=0.2
+    mask=imgNew.astype(bool)
+    out[mask]=cv2.addWeighted(img,alpha,imgNew,1-alpha,0)[mask]
+    return out
 
 class Button():
-    def __init__(self,pos,text,size=[45,45]):
+    def __init__(self,pos,text,size=[45,45], button_id=None, action=None):
         self.pos=pos
         self.size=size
         self.text=text
+        self.button_id = button_id  
+        self.action = action
            
 
 buttonList= []
@@ -37,43 +51,51 @@ for i in range(len(keys)):
         for j, key in enumerate(keys[i]):
             buttonList.append(Button([j*50+50,50*i+50],key))
 
+#Adding Clear Button
+buttonList.append(Button([350, 300], "CLEAR", size=[170, 50], button_id="CLEAR", action="CLEAR"))
+
+# Add Space Button
+buttonList.append(Button([100, 300], "SPACE", size=[170, 50], button_id="SPACE", action="SPACE"))
 
 while True:
     success, img =cap.read()
+    img=cv2.flip(img,1)
     hand,img=detector.findHands(img)
     img=drawALL(img,buttonList)
     
     if hand:
-        for button in buttonList:
-            x,y=button.pos
-            w,h=button.size
-            if x<hand[0][0]<x+w and y<hand[0][1]<y+h:
-                cv2.rectangle(img,button.pos,(x+w,y+h),(0,255,0),cv2.FILLED)
-                cv2.putText(img,button.text,(x+8,y+40),
-                        cv2.FONT_HERSHEY_PLAIN,
-                        3,(0,0,0),3)
-                if button.text=="Q":
-                    print("Q")
-                if button.text=="W":
-                    print("W")
-                if button.text=="E":
-                    print("E")
-                if button.text=="R":
-                    print("R")
-                if button.text=="T":
-                    print("T")
-                if button.text=="Y":
-                    print("Y")
-                if button.text=="U":
-                    print("U")
-                if button.text=="I":
-                    print("I")
-                if button.text=="O":
-                    print("O")
-                if button.text=="P":
-                    print("P")
-                if button.text=="
-
+        hand1=hand[0]
+        lmList=hand1["lmList"]
+        if lmList:
+            for button in buttonList:
+                x,y=button.pos
+                w,h=button.size
+                if x<lmList[8][0]<x+w and y<lmList[8][1]<y+h:
+                    cv2.rectangle(img,button.pos,(x+w,y+h),(0,0,0),cv2.FILLED)
+                    cv2.putText(img,button.text,(x+8,y+40),
+                            cv2.FONT_HERSHEY_PLAIN,
+                            3,(255,255,255),3)
+                    l,_,_=detector.findDistance(lmList[8][0:2],lmList[12][0:2],img)
+                
+                    #Button Press Code
+                    if l<30:
+                        cv2.rectangle(img,button.pos,(x+w,y+h),(0,255,0),cv2.FILLED)
+                        cv2.putText(img,button.text,(x+8,y+40),
+                                cv2.FONT_HERSHEY_PLAIN,
+                                3,(0,0,0),3)
+                        if button.action == "CLEAR":
+                            finalText = finalText[:-1]
+                        elif button.action == "SPACE":
+                            finalText += " "
+                        else:
+                            finalText += button.text
+                        sleep(0.3)
+                
+    cv2.rectangle(img,(50,200),(545,250),(0,0,0),cv2.FILLED)
+    cv2.putText(img,finalText,(55,240),
+                cv2.FONT_HERSHEY_PLAIN,
+                3,(255,255,255),3)            
+                
 
     cv2.imshow('Image',img)
     cv2.waitKey(1)
